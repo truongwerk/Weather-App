86dd7155764034a81a0fa17279032516d623617a
Tong Quang Truong <truongwerk@gmail.com>
2022-03-26 12:58:22 +0700

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,20 @@ const path = require("path");
 
 module.exports = {
 	entry: "./src/index.js",
+	mode: "production",
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+		],
+	},
 	output: {
-		filename: "main.js",
+		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist"),
 	},
 };
